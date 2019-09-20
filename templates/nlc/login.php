7d2ea6b5c22b804ffac2d577e21342438d3bf7ed
Setya Wibawa <durianpeople@gmail.com>
2019-09-20 22:46:18 +0700

    Update login.php

--- templates/nlc/login.php
+++ templates/nlc/login.php
@@ -7,6 +7,7 @@
     <title><?php if ($tmpl->title != "") echo $tmpl->title . " - "; ?><?php echo __SITENAME; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo $tmpl->url ?>/css/app2.css" />
+    <link rel="icon" href="<?php echo $tmpl->url?>/img/schematics.png" />
     <style>
         #loginCtn .helpform {
             display: none;
@@ -20,6 +21,7 @@
         <main>
             <div id="primary" class="p-t-b-100 height-full">
                 <img src="<?php h($tmpl->url)?>/img/object1.png" style="position: absolute;top: 100px; left: 50px;"/>
+                <img src="<?php h($tmpl->url)?>/img/object1.png" style="position: absolute;bottom: 100px; right: 50px;"/>
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-4 mx-md-auto paper-card">
