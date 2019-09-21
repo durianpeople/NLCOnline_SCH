35e4e45c5f033710a20f55aa9a25e474fb9a32ff
Setya Wibawa <durianpeople@gmail.com>
2019-09-21 09:13:32 +0700

    Update main.php

--- applications/nlc-online/view/user/petunjuk/main.php
+++ applications/nlc-online/view/user/petunjuk/main.php
@@ -11,11 +11,6 @@
             <li>- Sesi 5 (16.00 - 17.30)</li>
         </ul>
         <li>Setiap tim hanya diperbolehkan memilih salah satu sesi, dengan kuota setiap sesi sejumlah 300 tim</li>
-        <li>Setiap tim dapat segera memilih sesi dengan format login:</li>
-        <ul>
-            <li><b>Email: Email ketua tim</b></li>
-            <li><b>Password: Nomor HP ketua</b></li>
-        </ul>
         <li>Warm-up tidak termasuk dalam penilaian penyisihan Schematics NLC 2019.</li>
     </ol>
     <a href="/nlc/sesi" class="btn btn-primary">Daftar Sesi</a>
