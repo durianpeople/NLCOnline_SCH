4cfa5dacb5d954b478bcc632128baa995f1a270f
Setya Wibawa <durianpeople@gmail.com>
2019-09-22 18:12:18 +0700

    update

--- applications/nlc-online/menu.php
+++ applications/nlc-online/menu.php
@@ -9,6 +9,11 @@
         <span>Sesi</span>
         <i class="icon s-18 pull-right"></i>
     </a>
+    <a href="/nlc/nilai">
+        <i class="icon icon-documents3 blue-text s-18"></i>
+        <span>Nilai</span>
+        <i class="icon s-18 pull-right"></i>
+    </a>
     <?php if (PuzzleUser::isAccess(USER_AUTH_SU)) : ?>
         <a href="/admin">
             <i class="fas fa-user-shield" style="padding: 0 15px;width: 50px;text-align: center;"></i>
