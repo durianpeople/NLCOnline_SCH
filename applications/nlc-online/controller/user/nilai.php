367d50c9601e2afd0db1b3753639c0c662032fce
Setya Wibawa <durianpeople@gmail.com>
2019-09-23 10:31:23 +0700

    Update nilai.php

--- applications/nlc-online/controller/user/nilai.php
+++ applications/nlc-online/controller/user/nilai.php
@@ -5,27 +5,20 @@ use NLC\Base\Sesi;
 $sc = \Database::getRow("app_nlc_score", "user_id", PuzzleUser::active()->id);
 
 if ($sc) {
-    try {
-        $appProp->bundle['score'][] = [
-            "nama_sesi" => Sesi::load($sc['sesi_id'])->name,
-            "benar" => $sc['benar'],
-            "salah" => $sc['salah'],
-            "score" => $sc['score'],
-        ];
-    } catch (\Throwable $e) { 
-        $appProp->bundle['score'][] = [
-            "nama_sesi" => Sesi::load($sc['sesi_id'])->name,
-            "benar" => $sc['benar'],
-            "salah" => $sc['salah'],
-            "score" => $sc['score'],
-        ];
-    }
-} else {
     $appProp->bundle['score'][] = [
-        "nama_sesi" => '-',
-        "benar" => '-',
-        "salah" => '-',
-        "score" => '-',
+        "nama_sesi" => Sesi::load($sc['sesi_id'])->name,
+        "benar" => $sc['benar'],
+        "salah" => $sc['salah'],
+        "score" => $sc['score'],
+    ];
+} else {
+    $db = \Database::execute("SELECT s.`name` from app_nlc_sesi s INNER JOIN app_nlc_sesi_whitelist w on w.sesi_id = s.id and w.user_id = '?'", PuzzleUser::active()->id);
+    $row = $db->fetch_assoc();
+    $appProp->bundle['score'][] = [
+        "nama_sesi" => $row['name'],
+        "benar" => '0',
+        "salah" => '0',
+        "score" => '0',
     ];
 }
 return "main.php";
