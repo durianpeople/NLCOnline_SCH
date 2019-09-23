9c8aad43c62c4430f057bca0fe86db704810eea4
Setya Wibawa <durianpeople@gmail.com>
2019-09-23 09:32:09 +0700

    Update nilai.php

--- applications/nlc-online/controller/user/nilai.php
+++ applications/nlc-online/controller/user/nilai.php
@@ -4,11 +4,19 @@ use NLC\Base\Sesi;
 
 $sc = \Database::getRow("app_nlc_score", "user_id", PuzzleUser::active()->id);
 
-$appProp->bundle['score'][] = [
-    "nama_sesi" => Sesi::load($sc['sesi_id'])->name,
-    "benar" => $sc['benar'],
-    "salah" => $sc['salah'],
-    "score" => $sc['score'],
-];
-
+if ($sc) {
+    $appProp->bundle['score'][] = [
+        "nama_sesi" => Sesi::load($sc['sesi_id'])->name,
+        "benar" => $sc['benar'],
+        "salah" => $sc['salah'],
+        "score" => $sc['score'],
+    ];
+} else {
+    $appProp->bundle['score'][] = [
+        "nama_sesi" => '-',
+        "benar" => '-',
+        "salah" => '-',
+        "score" => '-',
+    ];
+}
 return "main.php";
